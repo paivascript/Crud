b443095dde638306d51b7b3f70d564dec1c5fc8d
paivascript <richardpaiva0101@gmail.com>
2026-04-08 19:04:21 -0300

    add controller e routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js"
-
+import routes from './routes/index.js'
 const conexao =  await conectaNaDatabase();
 
 conexao.on("error",(erro) => {
@@ -13,35 +12,7 @@ conexao.once("open",() =>{
 })
 
 const app = express();
-app.use(express.json());
-
-
-app.get("/", (req, res) =>{
-    res.status(200).send("Curso de Node.js");
-})
-
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-})
-
-app.get("/livros/:id", (req,res) => {
-    const index = buscarLivro(req.params.id)     
-    res.status(200).json(livros[index])
-})
-
-app.put("/livros/:id",(req,res) => {
-    const index = buscarLivro(req.params.id)
-    livros[index].titulo = req.body.titulo
-    res.status(200).json(livros);
-})
-
-app.post("/livros", (req,res) =>{
-    livros.push(req.body)
-    res.status(201).send("Livro cadastrado com sucesso!")
-
-})
+routes(app)
 
 export default app;
 
-//mongodb+srv://admin:xVCe3X5QF2lQVXjU@cluster0.kiutxr6.mongodb.net/?appName=Cluster0
