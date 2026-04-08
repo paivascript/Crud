e2920d248c29c1e153c1e03ad532824a3cf9d539
paivascript <richardpaiva0101@gmail.com>
2026-04-07 22:11:53 -0300

    criando crud

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id:1,
+        titulo:"Os 3 porquinhos"
+    },
+    {
+        id:2,
+       titulo: "Chapeuzinho Vermelho" 
+    }
+]
+
+function buscarLivro(id){
+   return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+}
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Curso de Node.js");
+})
+
+app.get("/livros",(req, res) => {
+    res.status(200).json(livros);
+})
+
+app.get("/livros/:id", (req,res) => {
+    const index = buscarLivro(req.params.id)     
+    res.status(200).json(livros[index])
+})
+
+app.put("/livros/:id",(req,res) => {
+    const index = buscarLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros);
+})
+
+app.post("/livros", (req,res) =>{
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso!")
+
+})
+
+export default app;
